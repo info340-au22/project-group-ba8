83fa91aef0228e1c5ef29cf0999484971201822b
dannyyue001 <dannyyue@uw.edu>
2022-10-23 23:46:51 -0700

    tryflex

--- js/scriptmain.js
+++ js/scriptmain.js
@@ -1,7 +1,7 @@
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
-    document.getElementById("eventbar").style.width = "500px";
-    document.getElementById("main").style.marginLeft = "500px";
+    document.getElementById("eventbar").style.width = "300px";
+    document.getElementById("main").style.marginLeft = "300px";
   }
   
   /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
