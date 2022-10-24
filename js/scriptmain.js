d67f7591fd094462a50658fa31838092ee11170b
dannyyue001 <dannyyue@uw.edu>
2022-10-23 21:01:19 -0700

    try

---
+++ js/scriptmain.js
@@ -0,0 +1,11 @@
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("eventbar").style.width = "500px";
+    document.getElementById("main").style.marginLeft = "500px";
+  }
+  
+  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+    document.getElementById("eventbar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
