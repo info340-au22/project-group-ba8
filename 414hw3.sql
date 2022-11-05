84469e8a9b082cdb7d8311ba120790330faa9a9e
annie <615287812@qq.com>
2022-11-05 03:52:01 -0700

    pixel art

---
+++ 414hw3.sql
@@ -0,0 +1,29 @@
+-- CREATE EXTERNAL DATA SOURCE flightdata_blob
+-- WITH (TYPE = BLOB_STORAGE, 
+-- LOCATION = 'https://introdatamanagement.blob.core.windows.net/flightdata'
+-- );
+
+CREATE TABLE CARRIERS (cid VARCHAR(7) PRIMARY KEY, name VARCHAR(83));
+
+CREATE TABLE MONTHS (mid INT PRIMARY KEY, month VARCHAR(9));
+
+CREATE TABLE WEEKDAYS (did INT PRIMARY KEY, day_of_week VARCHAR(9));
+
+CREATE TABLE FLIGHTS(fid INT PRIMARY KEY, 
+month_id INT REFERENCES MONTHS(mid), 
+day_of_month INT, 
+day_of_week_id INT REFERENCES WEEKDAYS(did),
+carrier_id VARCHAR(7) REFERENCES CARRIERS(cid), 
+flight_num INT,
+origin_city VARCHAR(34), 
+origin_state VARCHAR(47),
+dest_city VARCHAR(34), 
+dest_state VARCHAR(46), 
+departure_delay INT,
+taxi_out INT,
+arrival_delay INT,
+canceled INT, 
+actual_time INT, 
+distance INT, 
+capacity INT, 
+price INT);
