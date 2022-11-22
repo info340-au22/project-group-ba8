1847cb52d69df12ca1d6cdbd3eabdd1dbe75bf3c
dannyyue001 <dannyyue@uw.edu>
2022-11-21 20:57:35 -0800

    app basic set up

---
+++ src/components/scriptmain.js
@@ -0,0 +1,11 @@
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("eventbar").style.width = "300px";
+    document.getElementById("main").style.marginLeft = "300px";
+  }
+  
+  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+    document.getElementById("eventbar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
