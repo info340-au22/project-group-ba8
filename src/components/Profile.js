8394d0939cadc11a94beb96b70823d829b628fe5
annie <615287812@qq.com>
2022-11-23 04:37:10 -0800

    some profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,6 +2,18 @@ import React from "react";
 
 export default function Profile(props) {
     return (
-        <h1>This is Profile Page</h1>
+        //<h1>This is Profile Page</h1>
+        <div className="mx-5 mb-5">
+            <div className="container-fluid mt-4">
+                <div className="col-sm-12 col-lg-4 card-col">
+                    <div className="card my-2 p-3 rounded-0">
+                        <div className="card-body">
+                            <h2 id="personalPicture">Personal Info</h2>
+                            <img className="avatar" label="avatar" src="img/beaver.jpg" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
