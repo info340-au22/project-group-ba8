bba40fa80d85092f7ca2cd8dd718b4485114bef0
annie <615287812@qq.com>
2022-11-23 04:44:36 -0800

    little piece of profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -10,6 +10,27 @@ export default function Profile(props) {
                         <div className="card-body">
                             <h2 id="personalPicture">Personal Info</h2>
                             <img className="avatar" label="avatar" src="img/beaver.jpg" />
+                            <dl className="my-2">
+                                <dt className="col-sm-3">Name</dt>
+                                <dd className="col-sm-9">
+                                    <p>Beaver B. Beaver</p>
+                                </dd>
+
+                                <dt className="col-sm-3">Language</dt>
+                                <dd className="col-sm-9">
+                                    <p>English, French, Beaverish</p>
+                                </dd>
+
+                                <dt className="col-sm-3">Places</dt>
+                                <dd className="col-sm-9">
+                                    <p>Rivers, Forests, Woodcraft Fairs</p>
+                                </dd>
+
+                                <dt className="col-sm-3 text-truncate">Food</dt>
+                                <dd className="col-sm-9">
+                                    <p>Flowers, Berries, Fish</p>
+                                </dd>
+                            </dl>
                         </div>
                     </div>
                 </div>
