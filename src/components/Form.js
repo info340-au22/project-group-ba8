a99ad3e877fcd88b7842880ad16a869819023db3
VinceQian <qqian2@uw.edu>
2022-11-23 14:26:10 -0800

    add form callbacks

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,50 +1,89 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Form(props) {
+    const [name, setName] = useState('');
+    const [privacy, setPrivacy] = useState(false);
+    const [location, setLocation] = useState('');
+    const [date, setDate] = useState('');
+    const [time, setTime] = useState('');
+    const [intro, setIntro] = useState('');
+
+    const handleName = (event) => {
+        setName(event.target.value);
+    }
+
+    const handlePrivacy = (event) => {
+        setPrivacy(event.target.checked);
+    }
+
+    const handleLocation = (event) => {
+        setLocation(event.target.value);
+    }
+    const handleDate = (event) => {
+        setDate(event.target.value);
+    }
+    const handleTime = (event) => {
+        setTime(event.target.value);
+    }
+
+    const handleIntro = (event) => {
+        setIntro(event.target.value);
+    }
+
+    const handleSubmit = (event) => {
+        props.getFormCallback({name,privacy,location,date,time,intro});
+        event.preventDefault();
+    }
+
     return(
         <div className="col-xl-8">
-            <form className="row g-3">
+            <form className="row g-3" onSubmit={handleSubmit}>
                 <div className="col-md-9">
-                    <label for="event-input" className="form-label">Event:</label>
-                    <input type="text" className="form-control" id="event-input" placeholder="Consider a cool name.." required />
+                    <label htmlFor="event-input" className="form-label">Event:</label>
+                    <input type="text" className="form-control" id="event-input" 
+                    placeholder="Consider a cool name.." value={name} onChange={handleName} required />
                 </div>
 
                 <div className="col-md-3 align-self-end">
                     <div className="form-check form-switch">
-                        <input className="form-check-input" type="checkbox" role="switch" id="privacy-check" />
+                        <input className="form-check-input" type="checkbox" role="switch" id="privacy-check" onChange={handlePrivacy} />
                         <label className="form-check-label" htmlFor="privacy-check">Make it public</label>
                     </div>
                 </div>
 
                 <div className="col-12">
-                    <label for="location-input" className="form-label">Location:</label>
+                    <label htmlFor="location-input" className="form-label">Location:</label>
                     <div className="input-group">
-                        <input type="text" className="form-control" id="location-input" placeholder="Where is the event.." />
+                        <input type="text" className="form-control" id="location-input" 
+                        placeholder="Where is the event.." value={location} onChange={handleLocation} />
                         <button className="btn btn-outline-secondary" type="button"><i className="fa fa-map-marker" aria-label="map"></i></button>
                     </div>
                 </div>
 
                 <div className="col-md-6">
-                    <label for="date-input" className="form-label">Date:</label>
-                    <input type="date" id="date-Input" className="form-control" required />
+                    <label htmlFor="date-input" className="form-label">Date:</label>
+                    <input type="date" id="date-Input" className="form-control" 
+                    value={date} onChange={handleDate} required />
                 </div>
 
                 <div className="col-md-6">
-                    <label for="time-input" className="form-label">Time:</label>
-                    <input type="time" className="form-control" id="time-input" required />
+                    <label htmlFor="time-input" className="form-label">Time:</label>
+                    <input type="time" className="form-control" id="time-input" 
+                    value={time} onChange={handleTime} required />
                 </div>
 
                 <div className="col-12">
-                    <label for="intro-input" className="form-label">Introduction:</label>
-                    <textarea className="form-control" id="intro-input" placeholder="What it is.." required ></textarea>
+                    <label htmlFor="intro-input" className="form-label">Introduction:</label>
+                    <textarea className="form-control" id="intro-input" placeholder="What it is.." 
+                    value={intro} onChange={handleIntro} required ></textarea>
                 </div>
 
                 <div className="col-12">
-                    <label for="background-image" className="form-label">Background image</label>
+                    <label htmlFor="background-image" className="form-label">Background image</label>
                     <input type="file" className="form-control" id="background-image" />
                 </div>
 
-                <div class="d-grid gap-2 col-6 mx-auto mt-4">
+                <div className="d-grid gap-2 col-6 mx-auto mt-4">
                     <button className="btn btn-dark" type="submit">Add it to your plan!</button>
                 </div>
             </form>
--- src/components/Plan.js
+++ src/components/Plan.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "./Form";
 
 export default function Plan(props) {
+    const [eventObj,setEventObj] = useState({});
+
+    const getForm = (formObj) => {
+        setEventObj(formObj);
+    }
+
     return (
-    <div className="mx-5 my-4">
-        <div class="container">
-            <div class="row">
-                <Form />
-                <div class="d-none d-xl-block col-xl-4">
-                    <h1>Place for other components.</h1>
+    <div className="mx-4 my-4">
+        <div className="container">
+            <div className="row">
+                <Form getFormCallback={getForm}/>
+                <div className="d-none d-xl-block col-xl-4">
+                    <h1>{eventObj.name}</h1>
+                    <h1>{eventObj.privacy}</h1>
+                    <h1>{eventObj.location}</h1>
+                    <h1>{eventObj.date}</h1>
+                    <h1>{eventObj.time}</h1>
+                    <h1>{eventObj.intro}</h1>
                 </div>
             </div>
         </div>
