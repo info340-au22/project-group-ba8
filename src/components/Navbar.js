f945264d209f4e30fe04096828559afbcbbd73b2
dannyyue001 <102689763+dannyyue001@users.noreply.github.com>
2022-11-22 22:08:49 -0800

    router works

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,52 +1,55 @@
 import React from "react"; //import React library
-
+import {Link} from 'react-router-dom'
 const tabs = ["Home", "Plan", "Profile"];
 
 export default function Navbar(props) {
 
-    function handleClick(tabName) {
-        props.changeTabCallbk(tabName.toLowerCase());
-    }
-
-    const navItemsArr = tabs.map((tabName) => {
-        return (
-            <li className="nav-item" key={tabName.toLowerCase()}>
-                <button className="nav-link btn rounded-0" onClick={handleClick}>
-                    {tabName}
-                </button>
-            </li>
-        );
-    });
-
     return (
-    <nav className="navbar navbar-expand bg-info d-none d-lg-block" id="navbar">
-        <div className="container-fluid">
-        <ul className="navbar-nav p-2">
-            {navItemsArr}
-        </ul>
-        </div>
-    </nav>
-    );
+        <nav className="navbar navbar-expand bg-info d-none d-lg-block" id='navbar'>
+            <div className="container-fluid">
+                <ul className="navbar-nav p-2">
+                    <li >
+                       <img className="navicon" src="img/otter.png" />
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link active" aria-current="page" to="/home">
+                            Events Map
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="edit.html">
+                            Create Events
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/profile">
+                            My Profile
+                        </Link>
+                    </li>
+                    
+                </ul>
+            </div>
+        </nav>)
 }
-export  function navBot(props)
+export function navBot(props)
 {
    return (
     <nav className="nav-bar bg-dark fixed-bottom d-lg-none p-4 border-top-white">
         <ul className="nav justify-content-evenly">
             <li className="nav-item">
-                <a  href="edit.html">
+                <Link to ="/plan">
                     <i aria-label="add event"><img className="navicon" src="img/addeventicon.png" /></i>
-                </a>
+                </Link>
             </li>
             <li className="nav-item">
-                <a  aria-current="page" href="index.html">
+                <Link  aria-current="page" to="/home">
                     <i aria-label="homepage"><img className="navicon" src="img/homeicon.png" /></i>
-                </a>
+                </Link>
             </li>
             <li className="nav-item">
-                <a  href="profile.html">
+                <Link  to="/profile">
                     <i aria-label="my profile"><img className="navicon" src="img/profileicon.png" /></i>
-                </a>
+                </Link>
             </li>
         </ul>
     </nav>
