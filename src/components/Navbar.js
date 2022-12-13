bb1cc42d617fcc904a1eef538e24d5c279d8e327
Ricwxiao <cxiao4@uw.edu>
2022-12-12 20:29:20 -0800

    add media query for mobile navigation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,22 +8,22 @@ export default function Navbar(props) {
         <nav className="navbar navbar-expand bg-info d-lg-block" id='navbar'>
             <div className="container-fluid">
                 <ul className="navbar-nav p-1">
-                    <li className = "d-none d-sm-block">
-                       <img className="navicon" src="img/otterlarger.png" />
+                    <li>
+                       <img className="app-logo navicon" src="img/otterlarger.png" />
                     </li>
                     <li className="nav-item">
                         <Link className="nav-link" aria-current="page" to="/home">
-                           <p className = "navtext">Events Map</p> 
+                           <p className = "navtext"><i className="material-icons">receipt_long</i><span className="nav-label">Events Map</span></p>
                         </Link>
                     </li>
                     <li className="nav-item">
                         <Link className="nav-link" to="/plan">
-                        <p className = "navtext">Plan Events</p> 
+                        <p className = "navtext"><i className="material-icons">add_box</i><span className="nav-label">Plan Events</span></p>
                         </Link>
                     </li>
                     <li className="nav-item">
                         <Link className="nav-link" to="/profile">
-                        <p className = "navtext">My Profile</p> 
+                        <p className = "navtext"><i className="material-icons">account_circle</i><span className="nav-label">My Profile</span></p> 
                         </Link>
                     </li>
                 </ul>
