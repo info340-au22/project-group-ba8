5ba5e51b85cc51d5c222e452a4b5294081fda677
Ricwxiao <cxiao4@uw.edu>
2022-12-12 23:18:24 -0800

    finish functions of the profile edit popup

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -4,25 +4,53 @@ import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
 
 export function EditProfile(props) {
-  const [show, setShow] = useState(false);
+  const userProfile = props.userProfile;
 
-  const handleClose = () => setShow(false);
+  const [show, setShow] = useState(false);
+  const [nameInput, setNameInput] = useState('');
+  const [langInput, setLangInput] = useState('');
+  const [placesInput, setPlacesInput] = useState('');
+  const [foodInput, setFoodInput] = useState('');
+
+  const handleClose = () => {
+    setNameInput('');
+    setLangInput('');
+    setPlacesInput('');
+    setFoodInput('');
+    setShow(false)
+  }
   const handleShow = () => setShow(true);
 
-  let newName = '';
-  let newLanguage = '';
-  let newPlace = '';
-  let newFood = '';
+  function handleNameInput(evt) {
+    setNameInput(evt.target.value);
+  }
+  function handleLangInput(evt) {
+    setLangInput(evt.target.value);
+  }
+  function handlePlacesInput(evt) {
+    setPlacesInput(evt.target.value);
+  }
+  function handleFoodInput(evt) {
+    setFoodInput(evt.target.value);
+  }
+
   function handleSubmit() {
-    setShow(false);
-    // props.handleNameInput(newName);
-    // props.handleLangInput(newLanguage);
-    // props.handlePlacesInput(newPlace);
-    // props.handleFoodInput(newFood);
-    // let newName = '';
-    // let newLanguage = '';
-    // let newPlace = '';
-    // let newFood = '';
+    if (nameInput) {
+      userProfile.changeUserName(nameInput);
+    }
+    if (langInput) {
+      userProfile.changeUserLang(langInput);
+    }
+    if (placesInput) {
+      userProfile.changeUserPlaces(placesInput);
+    }
+    if (foodInput) {
+      userProfile.changeUserFood(foodInput);
+    }
+    setNameInput('');
+    setLangInput('');
+    setPlacesInput('');
+    setFoodInput('');
   }
 
   return (
@@ -36,46 +64,16 @@ export function EditProfile(props) {
           <Modal.Title>Edit Personal Profile</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Name</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="Type your name"
-                //value = {newName}
-                //onChange={props.handleNameInput(this)}
-                autoFocus
-              />
-            </Form.Group>
-
-            <Form.Group
-              className="mb-3"
-              controlId="exampleForm.ControlTextarea1"
-            >
-              <Form.Label>Language</Form.Label>
-              <Form.Control as="textarea" rows={1} 
-              placeholder="Language(s) that you speak"
-              //value = {newName}
-              />
-              
-            </Form.Group>
-
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Places</Form.Label>
-              <Form.Control
-                placeholder="Places you like"
-                //value = {newPlace}
-              />
-            </Form.Group>
-
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Food</Form.Label>
-              <Form.Control
-                placeholder="Food that your like"
-                //value = {newFood}
-              />
-            </Form.Group>
-          </Form>
+          <form>
+            <label htmlFor='nameInput'>Name</label>
+            <input type="text" className='form-control' id='nameInput' placeholder='Type your new name' value={nameInput} onChange={handleNameInput}/>
+            <label htmlFor='langInput'>Language</label>
+            <input type="text" className='form-control' id='langInput' placeholder='Type your languages' value={langInput} onChange={handleLangInput}/>
+            <label htmlFor='placesInput'>Places</label>
+            <input type="text" className='form-control' id='placesInput' placeholder='Type your favorite places' value={placesInput} onChange={handlePlacesInput}/>
+            <label htmlFor='foodInput'>Name</label>
+            <input type="text" className='form-control' id='foodInput' placeholder='Type your favorite food' value={foodInput} onChange={handleFoodInput}/>
+          </form>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,8 +1,5 @@
 import React, { useState } from 'react';
 import {AddNote} from "./AddNote";
-
-import {Example} from "./Popup";
-import Button from 'react-bootstrap/Button';
 import Note from './NoteCard';
 
 import { EditProfile } from "./Popup";
@@ -10,11 +7,6 @@ import { EditProfile } from "./Popup";
 
 export default function Profile(props) {
     const userProfile = props.userProfile;
-
-    const [userNameInput, setUserNameInput] = useState('');
-    const [userLangInput, setUserLangInput] = useState('');
-    const [userPlacesInput, setUserPlacesInput] = useState('');
-    const [userFoodInput, setUserFoodInput] = useState('');
 
     const [changeInfo, setChangeInfo] = useState(false);
 
@@ -30,42 +22,6 @@ export default function Profile(props) {
     //     let newNotes = [...allNotes, formObj];
     //     setAllNotes(newNotes);
     // }
-
-    // handle name input and change
-    function handleNameInput(evt) {
-        setUserNameInput(evt.target.value);
-    }
-    function handleNameChange(evt) {
-        userProfile.changeUserName(userNameInput);
-        setUserNameInput('');
-    }
-
-    // handle language input and change
-    function handleLangInput(evt) {
-        setUserLangInput(evt.target.value);
-    }
-    function handleLangChange(evt) {
-        userProfile.changeUserLang(userLangInput);
-        setUserLangInput('');
-    }
-
-    // handle places input and change
-    function handlePlacesInput(evt) {
-        setUserPlacesInput(evt.target.value);
-    }
-    function handlePlacesChange(evt) {
-        userProfile.changeUserPlaces(userPlacesInput);
-        setUserPlacesInput('');
-    }
-
-    // handle food input and change
-    function handleFoodInput(evt) {
-        setUserFoodInput(evt.target.value);
-    }
-    function handleFoodChange(evt) {
-        userProfile.changeUserFood(userFoodInput);
-        setUserFoodInput('');
-    }
 
     // Your Notes component (3rd column of the page)
     function YourNotes(props) {
@@ -105,7 +61,7 @@ export default function Profile(props) {
                             
 
                                 <h2 className="personalPicture">Personal Info <EditProfile show = {changeInfo} onHide = {() => setChangeInfo(false)} 
-                                handleNameInput = {handleNameInput} handleLangInput = {handleLangInput} handlePlacesInput = {handlePlacesInput} handleFoodInput = {handleFoodInput}/></h2>
+                                userProfile = {props.userProfile}/></h2>
                                 <img className="avatar my-3" label="avatar" src="img/beaver.jpg" alt="your user avatar" />
                                 <dl className="my-2">
                                     <div className="row-2 d-flex row mb-3">
@@ -142,17 +98,6 @@ export default function Profile(props) {
                                         <dd className="col">
                                             <p>{userProfile.userFood}</p>
                                         </dd>
-                                        {/* <div className='col-4'>
-                                            <button type="button" className='btn btn-outline-dark' data-bs-toggle="collapse" data-bs-target="#editFood" aria-expanded="false" aria-controls="editFood"><i className="material-icons">edit</i></button>
-                                        </div>
-                                        <div className="collapse" id="editFood">
-                                            <div className="input-group mb-3">
-                                                <input type="text" className="form-control" placeholder="Type your favortite foods" aria-label="Type your favortite foods" aria-describedby="basic-addon2" value={userFoodInput} onChange={handleFoodInput} />
-                                                <div className="input-group-append">
-                                                    <button className="btn btn-dark" type="button" onClick={handleFoodChange} data-bs-toggle="collapse" data-bs-target="#editFood"><i className="material-icons">done</i></button>
-                                                </div>
-                                            </div>
-                                        </div> */}
                                     </div>
                                 </dl>
                             </div>
