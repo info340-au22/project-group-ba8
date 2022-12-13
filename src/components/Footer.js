132dde628518f231103c86eeaf76bfae44e6f579
Ricwxiao <cxiao4@uw.edu>
2022-12-12 21:41:57 -0800

    updated contact info

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ export default function Footer(props) {
             {/* <div className="container-fluid"> */}
                 <div className="row contacts ms-5 d-flex">
                     <h1>Contacts</h1>
-                    <a href="mailto:groupba8@example.com">groupba8@example.com</a>
+                    <a href="mailto:dannyyue@uw.edu">dannyyue@uw.edu</a>
                 </div>
                 <p className="text-end">&copy; GROUP BA 8 2022</p>
             {/* </div> */}
